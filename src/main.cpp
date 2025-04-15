b281aa60fb82c71e39594ead214a21849dfb77f8
whacked <whacked@users.noreply.github.com>
2025-04-15 18:42:25 +0800

    working code to use with load cell + hx711 -> readouts

--- src/main.cpp
+++ src/main.cpp
@@ -1,27 +1,39 @@
 #include <Arduino.h>
+#include "HX711.h"
 
-// put function declarations here:
-int myFunction(int, int);
+HX711 scale;
+
+// Use the pins you wired
+#define DT 21
+#define SCK 22
 
 void setup()
 {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
-
   Serial.begin(115200);
   pinMode(2, OUTPUT);
+
+  scale.begin(DT, SCK);
+
+  Serial.println("Taring...");
+  scale.set_scale(); // default scale value
+  scale.tare();      // resets the scale to 0
+
+  for (int i = 0; i < 3; i++)
+  {
+    digitalWrite(2, HIGH);
+    delay(100);
+    digitalWrite(2, LOW);
+    delay(100);
+  }
+
+  Serial.println("Ready!");
 }
 
 void loop()
 {
-  digitalWrite(2, HIGH);
+  Serial.print("Raw: ");
+  Serial.print(scale.read());
+  Serial.print(" | Calibrated: ");
+  Serial.println(scale.get_units(), 2);
   delay(500);
-  digitalWrite(2, LOW);
-  delay(500);
-}
-
-// put function definitions here:
-int myFunction(int x, int y)
-{
-  return x + y;
 }
