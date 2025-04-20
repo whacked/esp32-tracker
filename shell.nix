65d9b01ec9b815ca937953b7fdca0cef551e22ba
whacked <whacked@users.noreply.github.com>
2025-04-20 18:20:49 +0800

    add shell.nix

---
+++ shell.nix
@@ -0,0 +1,48 @@
+{ pkgs ? import <nixpkgs> {} }:
+
+let
+  # provides "echo-shortcuts"
+  nix_shortcuts = import (pkgs.fetchurl {
+    url = "https://raw.githubusercontent.com/whacked/setup/refs/heads/master/bash/nix_shortcuts.nix.sh";
+    hash = "sha256-jCOOVA9rILbvYSKI6rAR/OdP1gv6jDFiiTG9FgJEnvg=";
+  }) { inherit pkgs; };
+
+  python = pkgs.python3Full;
+  pythonPackages = pkgs.python3Packages;
+
+in pkgs.mkShell {
+  buildInputs = [
+    # pkgs.websocat
+  ] ++ [
+    # (pkgs.poetry.override ({ python3 = python; }))
+    python
+    pythonPackages.ipython
+    pythonPackages.async-timeout
+    pythonPackages.numpy
+    pythonPackages.matplotlib
+    pythonPackages.pandas
+    pythonPackages.polars
+    pythonPackages.psutil
+    pkgs.mdsh
+    pkgs.pkg-config
+    pkgs.arrow-cpp          # C++ Arrow library
+    pkgs.fast-float
+  ] ++ (with pkgs; [
+    gnumake
+    gcc
+  ]) ++
+  nix_shortcuts.buildInputs
+  ;  # join lists with ++
+
+  nativeBuildInputs = [
+  ];
+
+  shellHook = nix_shortcuts.shellHook + ''
+    _setup-venv() {
+      pip install bleak
+    }
+    ensure-venv _setup-venv
+  '' + ''
+    echo-shortcuts ${__curPos.file}
+  '';  # join strings with +
+}
